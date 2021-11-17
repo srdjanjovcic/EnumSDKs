f71b3173f63d2a9e7e43447c4053e2071f509bf1
Srdjan Jovčić <srdjan@jovcic.net>
2021-11-17 10:03:21 -0800

    Add basic enumerator.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Build.Utilities;
 
 namespace EnumSDKs
 {
@@ -10,6 +7,19 @@ namespace EnumSDKs
     {
         static void Main(string[] args)
         {
+            var platformSDKs = ToolLocationHelper.GetTargetPlatformSdks();
+            foreach (var sdk in platformSDKs)
+            {
+                Console.WriteLine($"{sdk.TargetPlatformIdentifier} - {sdk.TargetPlatformVersion}");
+                var  platforms = ToolLocationHelper.GetPlatformsForSDK(sdk.TargetPlatformIdentifier, sdk.TargetPlatformVersion);
+                foreach (string platform in platforms)
+                {
+                    Console.WriteLine($"\t{platform}");
+                }
+            }
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
         }
     }
 }
